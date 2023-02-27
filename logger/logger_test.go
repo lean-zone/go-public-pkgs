2d3cbc1556165c9216a759473ef76ee3de02ecfe
lin.zhang@qizhidao.com <lin.zhang@qizhidao.com>
2023-02-27 11:22:07 +0800

    调整日志包布局

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -2,17 +2,16 @@
 // @E-mail: 1013851072@qq.com
 // @Create Time: UTC +8:00 2022/12/10 11:47:39
 
-package test
+package logger
 
 import (
-	"github.com/zhanglin-zl/go-public-pkgs/logger"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
-	log := logger.Default()
+	log := Default()
 	log.Info("test logger")
 
-	log.AddHook(logger.NewFileHookByTime("logs", "[goTest]", "log"))
+	log.AddHook(NewFileHookByTime("logs", "[goTest]", "log"))
 	log.Info("test file logger")
 }
